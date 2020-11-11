c837ab0b005597d452aa6d464b948af3f10fe925
shahrukh <shoaibtayyab121@gmail.com>
2020-11-12 01:06:37 +0530

    Enhancement

--- my_eb_site/settings.py
+++ my_eb_site/settings.py
@@ -140,9 +140,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # Add these new lines
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'my_eb_site/static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'my_eb_site/static')
+# ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
